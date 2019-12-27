483ed6d9c14bf9e0446d181ea217b7039fc3826d
Oleksandr Yakovenko <yakovenko9814@ukr.net>
2019-12-27 14:21:33 +0200

    02 Add Exhibition as entity

---
+++ src/main/java/rest/service/entity/Exhibition.java
@@ -0,0 +1,33 @@
+package rest.service.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Exhibition {
+    private Long id;
+    private String title;
+
+    public Exhibition() {
+    }
+
+    public Exhibition(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
